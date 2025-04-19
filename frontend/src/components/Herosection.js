ee2700a391af527bdef6aa8faf4937ae376d10a5
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-19 16:03:17 +0530

    delete

--- frontend/src/components/Herosection.js
+++
@@ -1,8 +0,0 @@
-const HeroSection = () => (
-    <section className="bg-red-600 text-white p-10 text-center">
-      <h1 className="text-4xl font-bold">Your Car, Our Priority</h1>
-      <p className="mt-4">Reliable repair services, 24/7 support.</p>
-    </section>
-  )
-  export default HeroSection
-  
