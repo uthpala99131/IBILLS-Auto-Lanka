86bf5213ada5376ae4d3a5521dc087ab7baa2801
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-20 20:42:32 +0530

    update footer

--- ibillsweb/src/components/Footer.jsx
+++ ibillsweb/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-6 mt-16">
-      <div className="max-w-7xl mx-auto px-4 flex justify-between">
-        <p>&copy; 2025 Cripar. All rights reserved.</p>
+    <footer className="py-6 mt-16 text-white bg-black">
+      <div className="flex justify-between px-4 mx-auto max-w-7xl">
+        <p>&copy; 2025 IBILLS AUTO LANKA. All rights reserved.</p>
         <p>Support: 24/7</p>
       </div>
     </footer>
