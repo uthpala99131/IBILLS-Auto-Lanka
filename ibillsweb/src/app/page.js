8d821d200fc7d429e965d0ae9890ba71e88edd3d
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-20 16:42:33 +0530

    add contact section link

--- ibillsweb/src/app/page.js
+++ ibillsweb/src/app/page.js
@@ -1,11 +1,12 @@
 import HeroSection from '../components/HeroSection';
 import VehicleBrands from '../components/VehicleBrands';
-
+import ContactDetails from '../components/ContactDetails';
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <VehicleBrands />
+      <ContactDetails />
     </main>
   );
 }
