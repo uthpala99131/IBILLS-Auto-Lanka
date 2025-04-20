8961b111111d5935fea7af44674953c5360d0f41
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-20 20:43:53 +0530

    spare parts section connect

--- ibillsweb/src/app/page.js
+++ ibillsweb/src/app/page.js
@@ -1,11 +1,14 @@
 import HeroSection from '../components/HeroSection';
 import VehicleBrands from '../components/VehicleBrands';
 import ContactDetails from '../components/ContactDetails';
+import Parts from '../components/SparepartsSection';
+
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <VehicleBrands />
+      <Parts />
       <ContactDetails />
     </main>
   );
