136ae4ea34561e6e347c58bb51c9e02d2660e756
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-23 08:42:17 +0530

    create jobcard page

--- ibillsweb/src/pages/jobcard.jsx
+++ ibillsweb/src/pages/jobcard.jsx
@@ -1,11 +1,19 @@
-import JobCardForm from '../components/JobCardForm';
-import '../app/globals.css';
+import JobCardForm from "../components/JobCardForm";
+import "../app/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function JobCardPage() {
   return (
-    <div className="min-h-screen p-6 text-white bg-black">
-      <h1 className="mb-4 text-3xl font-bold text-red-600">Create Job Card</h1>
-      <JobCardForm />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      <main className="flex-grow p-6 py-25 g-black ptext-white">
+        <h1 className="mb-4 text-3xl font-bold text-center text-red-600 ">Create Job Card</h1>
+        <JobCardForm />
+      </main>
+
+      <Footer />
     </div>
   );
 }
