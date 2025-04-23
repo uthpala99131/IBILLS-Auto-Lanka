2f7dc12563cd19522b0754efb9386f7e6cc9ce0c
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-23 19:17:41 +0530

    update jobcard page

--- ibillsweb/src/pages/jobcard.jsx
+++ ibillsweb/src/pages/jobcard.jsx
@@ -1,18 +1,27 @@
-import JobCardForm from "../components/JobCardForm";
-import "../app/globals.css";
+import '../app/globals.css';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { BookingForm } from '../components/BookingForm';
+import  { JobCard }  from '../components/jobcard/JobCard';
+import { ProgressTracker as Progress } from '../components/jobcard/ProgressTracker';
+import { InvoiceGenerator as Invoice } from '../components/jobcard/InvoiceGenerator';
+import { ScheduleDisplay as Schedule } from '../components/jobcard/ScheduleDisplay';
+import { ReviewSection as Review } from '../components/jobcard/ReviewSection';
 
-export default function JobCardPage() {
+export default function CustomerDashboard() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen text-center text-white bg-black">
       <Navbar />
-
-      <main className="flex-grow p-6 py-25 g-black ptext-white">
-        <h1 className="mb-4 text-3xl font-bold text-center text-red-600 ">Create Job Card</h1>
-        <JobCardForm />
+      <main className="flex-grow p-6">
+        <div className="max-w-screen-lg mx-auto space-y-10">
+          <BookingForm />
+          <JobCard />
+          <Progress />
+          <Invoice />
+          <Schedule />
+          <Review />
+        </div>
       </main>
-
       <Footer />
     </div>
   );
