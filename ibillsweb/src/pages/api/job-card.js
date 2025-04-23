2472b54582b25578ced1c7f8efd9c24b2a2f4c94
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-23 08:43:11 +0530

    create api/jobcard.js

---
+++ ibillsweb/src/pages/api/job-card.js
@@ -0,0 +1,15 @@
+import dbConnect from '../../lib/dbConnect';
+import JobCard from '../../models/JobCard';
+
+export default async function handler(req, res) {
+  await dbConnect();
+  if (req.method === 'POST') {
+    try {
+      const jobCard = new JobCard(req.body);
+      await jobCard.save();
+      res.status(200).json({ message: 'Success', jobCard });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+}
