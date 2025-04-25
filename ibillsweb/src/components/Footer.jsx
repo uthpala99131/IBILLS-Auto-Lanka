71f064d00c3af93218be74f5748377ad7b82b8fe
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-25 12:24:19 +0530

    remove margin top

--- ibillsweb/src/components/Footer.jsx
+++ ibillsweb/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-6 mt-16 text-white bg-black border-t border-gray-600">
+    <footer className="py-6 text-white bg-black border-t border-gray-600">
       <div className="flex justify-between px-4 mx-auto max-w-7xl">
         <p>&copy; 2025 IBILLS AUTO LANKA. All rights reserved.</p>
         <p>Support: 24/7</p>
