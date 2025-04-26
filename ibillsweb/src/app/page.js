061ac1a38803039aaa2c1bb7424903e2236c2caa
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-27 00:21:54 +0530

    update link  ServiceSection.jsx to page.js

--- ibillsweb/src/app/page.js
+++ ibillsweb/src/app/page.js
@@ -3,12 +3,14 @@ import VehicleBrands from '../components/VehicleBrands';
 import ContactDetails from '../components/ContactDetails';
 import Parts from '../components/SparepartsSection';
 import Recovery from '../components/RecoverySection';
+import Service from '../components/ServiceSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <Recovery />
+      <Service />
       <VehicleBrands />
       <Parts />
       <ContactDetails />
