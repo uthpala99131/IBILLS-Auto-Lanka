c5098da2abf1d00b22fa376d83b3ff559d447479
uthpala <uthpalaiabeysinghe99@gmail.com>
2025-06-04 08:26:53 +0530

    delete backend unused files

--- backend/routes/technicianRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const technicianController = require('../controllers/technicianController');
-
-// Technician routes
-router.get('/', technicianController.getAllTechnicians);
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-// POST /api/users/register
-router.post('/register', registerUser);
-
-export default router;
